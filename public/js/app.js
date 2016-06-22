badc406df4249a6b660cb1bb435d03ee9794ff94
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-06-21 20:20:52 -0400

    finished user story 7, 9, 10

--- public/js/app.js
+++ public/js/app.js
@@ -5,14 +5,23 @@ $(document).ready(function(){
   var $seconds = $('#seconds');
 
   $start.on('click', startCountdown);
+  $breakBtn.on('click', startBreak);
+
+  function startBreak(){
+    $minutes.text('05');
+    $seconds.text('00');
+    startCountdown();
+  }
 
   function startCountdown(){
     var countdown = setInterval(function(){
-      var secondsVal = +seconds.text();
-      var minutesVal = +minutes.text();
+      var secondsVal = +$seconds.text();
+      var minutesVal = +$minutes.text();
         if(secondsVal === 0 && minutesVal === 0){
           $breakBtn.removeClass('disabled');
           $breakBtn.removeAttr('disabled');
+          clearInterval(countdown);
+          return;
         }
         if(secondsVal === 0){
           $minutes.text(minutesVal - 1);
